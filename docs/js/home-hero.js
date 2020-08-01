1c759bd85a30ec04a57c00c64056fa922b4ce840
Rishi Keshav Pradeep <keshav.r.pradeep@gmail.com>
2020-08-01 01:02:48 -0700

    updates for students landing page

--- docs/js/home-hero.js
+++ docs/js/home-hero.js
@@ -8,7 +8,7 @@ window.addEventListener('scroll', function(){
 
     bg.style.top = -value * 0.05 + 'px';
     planet.style.left = -value * 0.5 + 'px';
-    planet.style.top = value * 0.7 + 'px';
+    planet.style.top = value * 0.8 + 'px';
     mountain.style.top = value * 0.35 + 'px';
     text.style.top = value + 'px';   
 

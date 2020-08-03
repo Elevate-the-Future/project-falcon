713e1f8b85b0982c2bec27ba16f3d8106c91dfa9
Rishi Keshav Pradeep <keshav.r.pradeep@gmail.com>
2020-08-03 16:11:06 -0700

    remove background from students page. Use moon to make it cleaner

--- docs/js/home-hero.js
+++ docs/js/home-hero.js
@@ -1,4 +1,4 @@
-let bg = document.getElementById('hero-bg');
+// let bg = document.getElementById('hero-bg');
 let planet = document.getElementById('hero-planet');
 let mountain_back = document.getElementById('hero-mountain-back');
 let mountain_middle = document.getElementById('hero-mountain-middle');
@@ -9,7 +9,7 @@ window.addEventListener('scroll', function(){
     var value = window.scrollY;
     
     // background
-    bg.style.top = -value * 0.2 + 'px';
+    // bg.style.top = -value * 0.2 + 'px';
     // planet
     planet.style.left = -value * 0.5 + 'px';
     planet.style.top = value * 0.8 + 'px';

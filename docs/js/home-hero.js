e77ef3e1a62ca53ff818d1bf97f097adbcec09b6
Rishi Keshav Pradeep <keshav.r.pradeep@gmail.com>
2020-08-03 15:46:26 -0700

    Update Master

--- docs/js/home-hero.js
+++ docs/js/home-hero.js
@@ -1,16 +1,23 @@
 let bg = document.getElementById('hero-bg');
 let planet = document.getElementById('hero-planet');
-let mountain = document.getElementById('hero-mountain');
+let mountain_back = document.getElementById('hero-mountain-back');
+let mountain_middle = document.getElementById('hero-mountain-middle');
+let mountain_front = document.getElementById('hero-mountain-front');
 let text = document.getElementById('hero-text');
 
 window.addEventListener('scroll', function(){
     var value = window.scrollY;
-
-    bg.style.top = -value * 0.05 + 'px';
+    
+    // background
+    bg.style.top = -value * 0.2 + 'px';
+    // planet
     planet.style.left = -value * 0.5 + 'px';
     planet.style.top = value * 0.8 + 'px';
-    mountain.style.top = value * 0.35 + 'px';
-    text.style.top = value + 'px';   
+    // mountains
+    mountain_back.style.top = value * 0.4 + 'px';
+    mountain_middle.style.top = value * 0.2 + 'px';
+    // text
+    text.style.top = value * 0.45 + 'px';   
 
 
 })

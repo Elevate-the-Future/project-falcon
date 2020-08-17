05f6ea76a8b217600e075e6d899348411d741343
keshprad <keshav.r.pradeep@gmail.com>
2020-08-17 13:29:43 -0700

    Update master

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,3 +1,4 @@
+// hero parallax
 let planet = document.getElementById('hero-planet');
 let mountain_back = document.getElementById('hero-mountain-back');
 let mountain_middle = document.getElementById('hero-mountain-middle');
@@ -18,3 +19,14 @@ window.addEventListener('scroll', function(){
 
 
 })
+
+// Scroll Partners
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
